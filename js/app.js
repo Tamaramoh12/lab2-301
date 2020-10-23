251a4d300ad38f86c1afb8c25e3f41ebd0151fa0
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2020-10-23 18:25:43 +0300

    enhance the code

--- js/app.js
+++ js/app.js
@@ -2,17 +2,17 @@
 
 let objsArr = [];
 let keywordsArr = [];
-let uniqeKeyWords = [];
+// let uniqeKeyWords = [];
 
 // Creating the cinstructor
-function Image(image){
+function Image(image){ //image is the JSON object
     this.image_url = image.image_url;
     this.title = image.title;
     this.description = image.description;
     this.keyword = image.keyword;
     this.horns = image.horns;
     objsArr.push(this);
-    keywordsArr.push(this.keyword);
+    // keywordsArr.push(this.keyword);
 }
 
 // Render the content using clone
@@ -30,28 +30,37 @@ Image.prototype.render = function(){
 }
 
 Image.readJson = () => {
-    const ajaxSetting = {
+    const ajaxSetting = { //ajaxSetting is object
         method: 'get',
         dataType: 'json'
     };
 
-    $.ajax('data/page-1.json' , ajaxSetting).then(data => {
-        data.forEach(item => {
-            let image = new Image(item);
+    $.ajax('data/page-1.json' , ajaxSetting).then(ourData => { //use the ajax(send ajax request) //data is an argument and we save the data inside it. 
+        ourData.forEach(item => { 
+            console.log(ourData);
+            //create object
+            let image = new Image(item); 
+            //add option and prevent dupliacte
+            if(! keywordsArr.includes(image.keyword)){
+                $('select').append(`<option value="${image.keyword}">${image.keyword}</option>`); 
+            keywordsArr.push(image.keyword);
+            }
+            
+            //render images
             image.render();
         });
-        fillTheSelect();
+        // fillTheSelect();
     });
 };
 
-function fillTheSelect(){
-    keywordsArr.forEach(function(value,index){
-        if(uniqeKeyWords.indexOf(value) === -1 ){
-            uniqeKeyWords.push(value);
-            $('select').append(`<option value="${value}">${value}</option>`);
-        } 
-    });
-}
+// function fillTheSelect(){
+//     keywordsArr.forEach(function(value,index){
+//         if(uniqeKeyWords.indexOf(value) === -1 ){
+//             uniqeKeyWords.push(value);
+//             $('select').append(`<option value="${value}">${value}</option>`);
+//         } 
+//     });
+// }
 
 $('select').click(function(){
     
@@ -61,5 +70,6 @@ $('select').click(function(){
     $(selectval).show();
 });
 
-//document.ready
+
+//document.ready ---> call the readJson function
 $(() => Image.readJson());
